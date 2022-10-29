632dd799fb21a48a33c2a353ad97709dbb3f2abe
ARTEM3221 <95755680+ARTEM3221@users.noreply.github.com>
2022-10-29 22:07:45 +0300

    3

--- js/main.js
+++ js/main.js
@@ -16,13 +16,20 @@ const logInForm = document.querySelector('#loginform');
 const loginInput = document.querySelector('#login');
 const userName = document.querySelector('.username');
 const buttonOut = document.querySelector('.button-out');
+const cardsRestaurants = document.querySelector('.rest-cards');
+const containerPromo = document.querySelector('.promo');
+const restaurants = document.querySelector('.restaurants');
+const menu = document.querySelector('.menu-cards');
+
+
+
 let login = localStorage.getItem('gloDelivery');
 function toggleModelAuth() {
-    modelAuth.classList.toggle("is-open");
-    if (modelAuth.classList.contains("model-auth")) {
-        disableScroll();
-    } else {
+    modelAuth.classList.toggle("is-open-auth");
+    if (modelAuth.classList.contains("is-open-auth")) {
         enableScroll();
+    } else {
+        disableScroll();
     }
 }
 
@@ -83,7 +90,7 @@ function notAuthorized() {
     closeAuth.addEventListener('click', toggleModelAuth);
     logInForm.addEventListener('submit', logIn);
     modelAuth.addEventListener('click', function (event) {
-        if (event.target.classList.contains('model-auth')) {
+        if (event.target.classList.contains("model-auth")) {
             toggleModelAuth();
         }
     })
@@ -98,3 +105,41 @@ function checkAuth() {
 }
 
 checkAuth();
+
+function createCardRestaurant() {
+
+    const card = `<div class="card">
+    <a><img src="images/card1.jpg" alt="" class="card-image"></a>
+    <div class="card-text">
+        <div class="card-head">
+            <h3 class="card-title">Піца плюс</h3>
+            <span class="card-tag">50 хв</span>
+        </div>
+        <div class="card-info">
+            <div class="raiting">&#9733; 4.5</div>
+            <div class="price">від 100 грн</div>
+            <div class="category">Піца</div>
+        </div>
+    </div>
+</div>
+`;
+    cardsRestaurants.insertAdjacentHTML('beforeend', card);
+
+}
+
+createCardRestaurant();
+createCardRestaurant();
+createCardRestaurant();
+
+function openGoods(event) {
+    const target = event.target;
+    const restaurant = target.closest('.rest-cards');
+
+    if (restaurant) {
+        containerPromo.classList.add('hide')
+        restaurants.classList.add('hide')
+        menu.classList.remove('hide')
+    }
+}
+
+cardsRestaurants.addEventListener('click', openGoods);
